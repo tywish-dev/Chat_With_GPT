70478f18ab0fe2c93c7c32a306e5ef93b891ed35
Samet Yılmaz <55537987+tywish-dev@users.noreply.github.com>
2023-03-11 12:14:01 +0300

    added project files

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'ui/view/screens/chat_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const ChatPage(),
+    );
+  }
+}
---
+++ lib/ui/view/widgets/custom_app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key, required this.appBar});
+  final AppBar appBar;
+  @override
+  Size get preferredSize => Size.fromHeight(appBar.preferredSize.height);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.network(
+            "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/chatgpt-icon.png",
+            width: 40,
+            height: 40,
+          ),
+          const Text(
+            'ChatGPT',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            textAlign: TextAlign.center,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+          )
+        ],
+      ),
+    );
+  }
+}
